3d240b6497f24ef50c6d6779363fe28414a9b68c
sawdareh <dareh@kesan.asia>
2025-06-20 17:29:16 +0700

    Remove service_key.json and add to .gitignore

--- src/app/actions/saveDepartment.ts
+++ src/app/actions/saveDepartment.ts
@@ -3,19 +3,17 @@
 import {eq} from 'drizzle-orm'
 import {flattenValidationErrors} from 'next-safe-action'
 import {redirect} from 'next/navigation'
-
 import {db} from '@/db'
 import {departments} from '@/db/schema'
 import {actionClient} from '@/lib/safe-action'
 import { insertDepartmentSchema,type insertDepartmentSchemaType } from '@/zod-schemas/department'
-
 import {getKindeServerSession} from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const saveDepartmentAction=actionClient
     .metadata({actionName:'saveDepartmentNameAction'})
     .schema(insertDepartmentSchema,{
         handleValidationErrorsShape:async (ve)=>flattenValidationErrors(ve).fieldErrors,
-
+ 
     })
     .action(async ({
         parsedInput:department
